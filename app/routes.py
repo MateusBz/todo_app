94da625a2378f36e0a94e66404a2fa5e1c6b4e25
mateusz <mateusz.bzdzion@gmail.com>
2020-02-02 17:14:50 +0100

    create validate for forms, flash messages

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
-
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +13,11 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, tasks=tasks)
 
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user{form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
+    return render_template('login.html', form=form, title='Sign In')
